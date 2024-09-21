6591b7b9aac0be912ceeee8f80ffe47d78fb42ce
sozge <sozgeozkartal@gmail.com>
2024-09-22 00:25:00 +0300

    QrCodeFragment xml page finished.

--- app/src/main/java/com/sozge/qryzer/QrCodeFragment.kt
+++ app/src/main/java/com/sozge/qryzer/QrCodeFragment.kt
@@ -1,17 +1,22 @@
 package com.sozge.qryzer
 
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.sozge.qryzer.databinding.FragmentQrCodeBinding
 
 class QrCodeFragment : Fragment() {
     private var _binding: FragmentQrCodeBinding? = null
     private val binding get() = _binding!!
 
-
+    companion object {
+        private const val CAMERA_PERMİSSİON_REQUEST_CODE = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +28,29 @@ class QrCodeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {_binding = FragmentQrCodeBinding.inflate(inflater,container, false)
         val view = binding.root
-        return view }
+        return view
+        }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        checkForPermission()
+    }
+
+    private fun checkForPermission() {
+        if(ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED) {
+            scanerFragment()
+        }else
+            requestThePermission()
+
+    }
+    private fun requestThePermission(){
+        ActivityCompat.requestPermissions(
+            requireContext() as Activity, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_PERMİSSİON_REQUEST_CODE)
+    }
+
+    private fun scanerFragment(){
+
+    }
 }
